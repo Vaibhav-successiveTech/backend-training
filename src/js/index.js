3e1eb7fd70c8dd0e4bf9f5d43c8dbe59336c7838
Vaibhav <vaibhav.sharma@successive.tech>
2025-07-31 12:47:46 +0530

    seperated js and ts code

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+import mongoose from 'mongoose';
+import ListServiceObject from './services/list';
+import ListControllerObject from './controller/list';
+
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/successive-db');
+const db = mongoose.connection;
+
+db.once('open', () => console.log('MongoDB connected'));
+
+app.post('/db/list', ListServiceObject.ListService,ListControllerObject.ListController);
+
+app.listen(3032, () => {
+    console.log('server listening on https://localhost:3032');
+});
