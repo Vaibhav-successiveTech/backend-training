f24f0a68a01377d505cdb03cfdb63ee54f33832d
Vaibhav <vaibhav.sharma@successive.tech>
2025-07-31 12:55:18 +0530

    seperated js and ts code

---
+++ src/js/app.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import UserRoutes from "./routes/userRoutes";
+
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/userPage');
+const db = mongoose.connection;
+
+db.once('open', () => console.log('Mongoose Connected'));
+
+app.use('/Day10/user',UserRoutes);
+
+app.use((err, req, res, next) => {
+    res.status(400).json({
+        message : err.message
+    })
+})
+
+app.listen(3000, () => {
+    console.log('listening on https://localhost:3000');
+})
---
+++ src/js/middleware/userLogin.js
@@ -0,0 +1,41 @@
+import zod from 'zod';
+import User from '../ts/models/user';
+import bcrypt from 'bcrypt';
+import { NextFunction, Request, Response } from 'express';
+import createError from 'http-errors';
+
+const UserLoginSchema = zod.object({
+    userName: zod.string(),
+    password: zod.string()
+});
+
+class UserLoginMiddlewwareClass {
+    UserLoginMiddleware = async (req, res, next) => {
+        const result = UserLoginSchema.safeParse(req.body);
+
+        if (!result.success) {
+            next(createError(400, result.error));
+        }
+        try {
+            const { userName, password } = req.body;
+            const dbResult = await User.findOne({ userName });
+            if (!dbResult) {
+                next(createError(400, 'No user Exist'))
+                return;
+            }
+            const dbPass = dbResult.hashedPassword || '';
+            const isMatch = await bcrypt.compare(password, dbPass);
+            if (!isMatch) {
+                next(createError(401, 'Invalid Username or Password'))
+                return;
+            }
+            req.body.result = dbResult;
+            next();
+        } catch (err) {
+            next(createError(401, String(err)))
+        }
+    }
+}
+
+const UserLoginMiddlewwareObject = new UserLoginMiddlewwareClass();
+export default UserLoginMiddlewwareObject;
