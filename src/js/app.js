8506ffb8b8d903b6cddec66b50360248daa2b1da
Vaibhav <vaibhav.sharma@successive.tech>
2025-07-31 12:36:55 +0530

    seperated js and ts code

---
+++ src/js/app.js
@@ -0,0 +1,18 @@
+import express, { NextFunction, Request, Response } from 'express';
+import AllRouter from './routes/Allroutes';
+const app = express();
+app.use(express.json());
+
+ app.use('/Day5',AllRouter);
+
+
+app.use((err,req,res,next)=>{
+    res.status(400).json({
+        error : err.message
+    })
+});
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT,()=>{
+    console.log(`Server Listening on https://localhost:${PORT}`);
+});
