37a230e1c929d2ec35419aaf7945eada3c36b777
Vaibhav <vaibhav.sharma@successive.tech>
2025-08-07 15:38:11 +0530

    mongodb assessment

--- src/aggregation/Question2.ts
+++
@@ -1,8 +0,0 @@
-import OrderModel from "../models/sampleData"
-const NumberOfOrder = async()=>{
-    const resp = await OrderModel.aggregate([{$group : {_id: '$status',itemCount : {$sum:1}}}])
-    console.log(`\nStatus : `);
-    resp.map((i)=>console.log(i));
-}
-
-export default NumberOfOrder;
--- src/aggregation/Question3.ts
+++
@@ -1,7 +0,0 @@
-import OrderModel from "../models/sampleData";
-const Top3Customers = async () => {
-    const top3 = await OrderModel.aggregate([{ $sort: {totalAmount : -1} },{$limit:3}])
-    console.log(`\nTop 3 Customers : ${(top3[0]?.customerName)},${(top3[1]?.customerName)},${(top3[2]?.customerName)}`)
-}
-
-export default Top3Customers;
