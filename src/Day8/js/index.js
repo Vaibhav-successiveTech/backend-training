2e7cdc08d65374fad1c4cfb6f903776b2c7719e6
Vaibhav <vaibhav.sharma@successive.tech>
2025-08-12 10:37:27 +0530

    added env

--- src/Day8/js/index.js
+++ src/Day8/js/index.js
@@ -2,18 +2,22 @@ const express = require('express');
 import mongoose from 'mongoose';
 import ListServiceObject from './services/list';
 import ListControllerObject from './controller/list';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 
 const app = express();
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/successive-db');
+const URL = process.env.mongoURL;
+mongoose.connect(URL);
 const db = mongoose.connection;
 
 db.once('open', () => console.log('MongoDB connected'));
 
 app.post('/db/list', ListServiceObject.ListService,ListControllerObject.ListController);
 
-app.listen(3032, () => {
-    console.log('server listening on https://localhost:3032');
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`server listening on https://localhost:${PORT}`);
 });
